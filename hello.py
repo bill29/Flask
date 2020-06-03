2d441a867b5dbce90438cc0bf87966d652762e70
minh_dan <dan29121998@gmail.com>
2020-06-03 17:56:33 +0700

    buoi1

--- hello.py
+++ hello.py
@@ -1,15 +1,33 @@
-from flask import Flask, redirect, url_for
-app = Flask(__name__)
-
-@app.route('/hello/<name>')
-def hello_world(name):
-    return 'hello world, %s' %name
-@app.route('/goodbye/<name>')
-def good_bye(name):
-    return  'Goodbye ' + name
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-    return 'Blog number %d' %postID
-app.add_url_rule('/nguyenminhdan', 'hello', hello_world)
-if __name__ == '__main__':
-    app.run(debug=True)
+# from flask import Flask, redirect, url_for
+# app = Flask(__name__)
+#
+# @app.route('/admin')
+# def hello_admin():
+#    return 'Hello Admin'
+#
+# @app.route('/guest/<guest>')
+# def hello_guest(guest):
+#    return 'Hello %s as Guest' % guest
+#
+# @app.route('/user/<name>')
+# def hello_user(name):
+#    if name =='admin':
+#       return redirect(url_for('hello_admin'))
+#    else:
+#       return redirect(url_for('hello_guest',guest = name))
+#
+# @app.route('/music')
+# def music():
+#     return 'MUSIC'
+# @app.route('/sport')
+# def sport():
+#     return 'SPORT'
+# @app.route('/favourite/<name>')
+# def favourite(name):
+#     if name=='music':
+#         return redirect(url_for('music'))
+#     else:
+#         return redirect(url_for('sport'))
+#
+# # if __name__ == '__main__':
+# #    app.run(debug = True)
